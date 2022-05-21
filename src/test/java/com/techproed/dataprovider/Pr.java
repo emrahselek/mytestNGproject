00dbc75a3b8faec84d6e2916ae02349ec8d2c59d
emrahselek <emahselek10@gmail.com>
2022-05-21 07:10:24 -0600

    ed

--- src/test/java/com/techproed/dataprovider/Pr.java
+++ src/test/java/com/techproed/dataprovider/Pr.java
@@ -1,5 +1,23 @@
 package com.techproed.dataprovider;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class Pr {
+
+    @Test(dataProvider = "dataset")
+    public void test(String username, String password, String age){
+        System.out.println(username+" "+password+" "+age);
+
+    }
+
+    @DataProvider
+    public Object[][] dataset() {
+        return new Object[][]{
+                {"user1", "pass1", "30"},
+                {"user2", "pass2", "25"},
+                {"user3", "pass3", "65"}
+        };
+    }
 
 }
